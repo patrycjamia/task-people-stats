c9a33166e9dff1d8b1fbeb3cd95cedd5ce5887ab
Patrycja Miazek <patrycjamiazek17@gmail.com>
2020-10-19 20:15:49 +0200

    szosty

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        public static void main (String[]args){
+            PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
+            System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+        }
+
     }
 }
 
@@ -17,11 +23,16 @@ class PeopleStats {
             throw new IllegalStateException(e);
         }
     }
+
+    public long count() {
+        return people.size();
+    }
 }
 
 class Person {
     private final String firstName;
-    private final String last Name;
+    private final String last
+    Name;
 
     public Person(String firstName) {
         this.firstName = firstName;
